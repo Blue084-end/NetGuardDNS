7dd927276eb6b81333034b316d2fd1d24d4d5aa9
Blue084-end <azend11110000@gmail.com>
2025-09-11 21:33:32 +0700

    Update AppControlViewModel.java

--- app/src/main/java/eu/faircode/netguard/viewmodel/AppControlViewModel.java
+++ app/src/main/java/eu/faircode/netguard/viewmodel/AppControlViewModel.java
@@ -1,1 +1,60 @@
 
+package eu.faircode.netguard.viewmodel;
+
+import android.app.Application;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.faircode.netguard.RulesManager;
+
+public class AppControlViewModel extends AndroidViewModel {
+
+    private final MutableLiveData<List<ApplicationInfo>> appListLiveData = new MutableLiveData<>();
+
+    public AppControlViewModel(@NonNull Application application) {
+        super(application);
+        loadInstalledApps();
+    }
+
+    private void loadInstalledApps() {
+        PackageManager pm = getApplication().getPackageManager();
+        List<ApplicationInfo> allApps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        List<ApplicationInfo> userApps = new ArrayList<>();
+
+        for (ApplicationInfo appInfo : allApps) {
+            if ((appInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+                userApps.add(appInfo);
+            }
+        }
+
+        appListLiveData.setValue(userApps);
+    }
+
+    public LiveData<List<ApplicationInfo>> getAppList() {
+        return appListLiveData;
+    }
+
+    public boolean isWifiAllowed(String packageName) {
+        return RulesManager.isWifiAllowed(getApplication(), packageName);
+    }
+
+    public boolean isMobileAllowed(String packageName) {
+        return RulesManager.isMobileAllowed(getApplication(), packageName);
+    }
+
+    public void setWifiAllowed(String packageName, boolean allowed) {
+        RulesManager.setWifiAllowed(getApplication(), packageName, allowed);
+    }
+
+    public void setMobileAllowed(String packageName, boolean allowed) {
+        RulesManager.setMobileAllowed(getApplication(), packageName, allowed);
+    }
+}
