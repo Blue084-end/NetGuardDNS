63067e60ccccece29fbfd04041376e6f63af0100
Blue084-end <azend11110000@gmail.com>
2025-09-11 18:29:19 +0700

    Update DnsServer.java

--- app/src/main/java/eu/faircode/netguard/dnsfilter/DnsServer.java
+++ app/src/main/java/eu/faircode/netguard/dnsfilter/DnsServer.java
@@ -7,3 +7,27 @@ public class DnsServer {
         return DnsResolver.query(domain);
     }
 }
+
+////bo sung
+
+public class DnsServer {
+    private static boolean running = false;
+
+    public static void start() {
+        if (running) return;
+        running = true;
+        // TODO: Khởi động DNS proxy, lắng nghe truy vấn
+    }
+
+    public static void stop() {
+        running = false;
+        // TODO: Dừng DNS proxy
+    }
+
+    public static InetAddress resolve(String domain) {
+        if (FilterList.isBlocked(domain)) {
+            return null;
+        }
+        return DnsResolver.query(domain);
+    }
+}
